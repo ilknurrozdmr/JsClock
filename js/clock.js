e461de802c92d5b00a940216f883edfe5e95910d
ilknurrozdmr <43750397+ilknurrozdmr@users.noreply.github.com>
2021-11-18 16:03:09 +0300

    first commit

---
+++ js/clock.js
@@ -0,0 +1,18 @@
+let Name = prompt("Adınızı Giriniz");
+let myName= document.querySelector("#myName")
+
+myName.innerHTML = `${Name.toUpperCase()}!`
+
+
+function tarihSaat() {
+    var date = new Date().toLocaleString();
+    document.querySelector("#myClock").innerHTML = date;
+}
+
+
+const d = new Date();
+const days = ["PAZAR", "PAZARTESİ", "SALI", "ÇARŞAMBA", "PERŞEMBE", "CUMA", "CUMARTESİ","PAZAR"];
+document.querySelector("#myDay").innerHTML = days[d.getDay()];
+
+
+setInterval(tarihSaat, 1000); 
